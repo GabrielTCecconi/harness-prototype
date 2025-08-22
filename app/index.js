0e1b2253971e138062a685ca576b050be2f0183d
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-08-22 14:00:17 -0300

    teste

--- app/index.js
+++ app/index.js
@@ -1,8 +1,7 @@
 export const handler = async (event) => {
-  const name = event?.queryStringParameters?.name ?? "world";
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: `Hello, ${name}!` }),
+    body: JSON.stringify({ message: `Hello, World! Teste 2` }),
   };
 };
